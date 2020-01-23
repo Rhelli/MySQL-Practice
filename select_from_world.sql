b009f449e9b46fa195487f15912e96858164f0e7
Rory Hellier <rory.hellier@googlemail.com>
2020-01-23 17:35:20 -0500

    Finished adding all sql queries for the select_from_world section SQLZoo. Merging into develop

--- select_from_world.sql
+++ select_from_world.sql
@@ -48,3 +48,18 @@ WHERE gdp >= Power(10, 12);
 SELECT name, capital
 FROM world
 WHERE LENGTH(name) = LENGTH(capital);
+
+-- 11. Show the name and the capital where the first letters of each match (N.B not when they are the same)
+SELECT name, capital
+FROM world
+WHERE LEFT(name, 1) = LEFT(capital, 1) AND name <> capital;
+
+-- 12. Find the country that has all the vowels and no spaces in it's name
+SELECT name
+FROM world
+WHERE name NOT LIKE '% %'
+AND name LIKE '%a%'
+AND name LIKE '%e%'
+AND name LIKE '%i%'
+AND name LIKE '%o%'
+AND name LIKE '%u%';
